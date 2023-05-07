2741ebec2512b2fc21fd6e92260ad667da1ceccb
MD-SUMON-MIAH <sumonmiah2018331062@gmail.com>
2023-05-07 10:52:57 +0600

    uploaded

---
+++ src/main/java/com/entity/Cid.java
@@ -0,0 +1,20 @@
+package com.entity;
+
+public class Cid {
+	private int c_id;
+	public Cid() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Cid(int c_id) {
+		super();
+		this.c_id=c_id;
+	}
+	
+
+   public int getc_id()
+   {
+	   return c_id;
+   }
+	
+}
